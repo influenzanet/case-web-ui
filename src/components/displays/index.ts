f33a1733aab699396aa840ead8d9cd688275dbef
phev8 <phev8@users.noreply.github.com>
2021-03-05 21:06:25 +0100

    export displays

---
+++ src/components/displays/index.ts
@@ -0,0 +1,21 @@
+import AccordionList from './AccordionList';
+import AlertBox from './AlertBox';
+import ImageContainer from './ImageContainer';
+import LoadingPlaceholder from './LoadingPlaceholder';
+import LogoCredits from './LogoCredits';
+import MarkdownRenderer from './MarkdownRenderer';
+import TeaserImage from './TeaserImage';
+import TitleBar from './TitleBar';
+import VideoPlayer from './VideoPlayer';
+
+export {
+  AccordionList,
+  AlertBox,
+  ImageContainer,
+  LoadingPlaceholder,
+  LogoCredits,
+  MarkdownRenderer,
+  TeaserImage,
+  TitleBar,
+  VideoPlayer,
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './components/cards';
 export * from './components/charts';
 export * from './components/dataLoaders';
 export * from './components/dialogs';
+export * from './components/displays';
 export * from './components/footer';
 export * from './components/header';
 export * from './components/misc';
