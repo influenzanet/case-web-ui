30dc7152b4f94bd900edb0bd8ee841c74e4666de
phev8 <phev8@users.noreply.github.com>
2021-03-17 21:31:39 +0100

    Add storybook example for accordionList

---
+++ src/components/displays/stories/AccordionList.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import AccordionList from '../AccordionList';
+
+import 'localStyles';
+
+export default {
+  title: "Displays/Accordion List"
+};
+
+export const Example = () => <AccordionList
+  items={[
+    {
+      title: 'Item 1 title',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusantium quas ab libero nisi nam, reiciendis placeat enim qui eligendi dolore facilis quod illo soluta quaerat iusto adipisci reprehenderit cum.'
+    },
+    {
+      title: 'Item 2 title with much longer description to see what happens when text does not fit into one line any more, when displaying it on typical screensizes',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem accusantium quas ab libero nisi nam, reiciendis placeat enim qui eligendi dolore facilis quod illo soluta quaerat iusto adipisci reprehenderit cum.'
+    }
+  ]}
+  openLabel="Show more"
+  closeLabel="Show less"
+/>
