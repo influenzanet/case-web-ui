bc451d6c892514efe677d11f269fd79eac51290a
Clément Turbelin <clement.turbelin@gmail.com>
2022-08-18 13:56:28 +0200

    export CustomSurveyResponseComponent

--- src/components/survey/index.ts
+++ src/components/survey/index.ts
@@ -1,11 +1,12 @@
 import SurveySingleItemView from './SurveySingleItemView/SurveySingleItemView';
 import SurveyPageView from './SurveyView/SurveyPageView/SurveyPageView';
 import SurveyView from './SurveyView/SurveyView';
-
+export type { CustomSurveyResponseComponent } from './SurveySingleItemView/ResponseComponent/ResponseComponent';
 export {
   SurveyView,
   SurveySingleItemView,
   SurveyPageView,
+
 }
 
 export * from "./SurveySingleItemView/utils";
