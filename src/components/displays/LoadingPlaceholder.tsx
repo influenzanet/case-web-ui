bca6ea344112f268d3728e54b9dc1c4b221ee6de
Matteo Delfino <matteo.delfino@isi.it>
2023-07-16 16:47:47 +0200

    LoadingPlaceholder: support className
    this is useful for applying external styling

--- src/components/displays/LoadingPlaceholder.tsx
+++ src/components/displays/LoadingPlaceholder.tsx
@@ -7,6 +7,7 @@ interface LoadingPlaceholderProps {
   color: 'primary' | 'secondary' | 'white';
   iconSize?: string;
   iconBorderWidth?: string;
+  className?: string;
 }
 
 const defaultIconSize = '3rem';
@@ -18,6 +19,7 @@ const LoadingPlaceholder: React.FC<LoadingPlaceholderProps> = (props) => {
   return (
     <div
       className={clsx(
+        props.className,
         "loading-placeholder d-flex align-items-center justify-content-center",
         {
           'vh-100': props.minHeight === 'vh-100',
